31afabbbdb7b3b9f66d0eb1eb444fedf87fb5279
Vira Voronova <vera.voronova@hotmail.com>
2022-08-23 20:34:22 +0200

    modal markup added

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,16 +1,16 @@
-// {
-//   const refs = {
-//     openModalBtn: document.querySelector("[data-modal-open]"),
-//     closeModalBtn: document.querySelector("[data-modal-close]"),
-//     modal: document.querySelector("[data-modal]"),
-//   };
+{
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
 
-//   refs.openModalBtn.addEventListener("click", toggleModal);
-//   refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
 
-//   function toggleModal() {
-//     refs.modal.classList.toggle("backdrop--is-hidden");
-//     document.body.classList.toggle("modal-open");
-//   }
-// };
+  function toggleModal() {
+    refs.modal.classList.toggle("backdrop--is-hidden");
+    document.body.classList.toggle("modal-open");
+  }
+};
 
