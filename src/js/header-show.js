28d48015263c4131531d58caede510aebd9e3774
Vira Voronova <vera.voronova@hotmail.com>
2022-08-25 10:49:00 +0200

    header js

--- src/js/header-show.js
+++ src/js/header-show.js
@@ -1,4 +1,28 @@
+const refs = {
+  header: document.querySelector('.header'),
+}
 
-/* 
+let lastScroll = 0; // previous scroll position
+const defaultOffset = 400;
 
-*/
+window.addEventListener('scroll', onWindowScroll)
+
+// current scroll position
+const scrollPosition = () => window.pageYOffset || document.documentElement.scrollTop
+
+// if header contains class "is-hidden"
+const containHidden = () => refs.header.classList.contains('header--is-hidden')
+
+
+function onWindowScroll() {
+  if(scrollPosition() > lastScroll && !containHidden() && scrollPosition() > defaultOffset) {
+    // scroll down
+    refs.header.classList.add('header--is-hidden')
+
+  } else if (scrollPosition() < lastScroll && containHidden()) {
+    // scroll up
+    refs.header.classList.remove('header--is-hidden')
+  }
+
+  lastScroll = scrollPosition();
+}
