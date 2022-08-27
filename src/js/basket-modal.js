4a42f8f62b5f609de085bca6640f692b2f6ee78b
Vira Voronova <vera.voronova@hotmail.com>
2022-08-27 14:05:16 +0200

    basket fixed

--- src/js/basket-modal.js
+++ src/js/basket-modal.js
@@ -4,12 +4,14 @@ import { openModal, closeModal, onBackdropClick } from "./modal.js";
     openModalBtn: document.querySelector("[data-basket-modal-open]"),
     openModalMobileBtn:document.querySelector('[data-basket-mobile-modal-open]'),
     closeModalBtn: document.querySelector("[data-basket-modal-close]"),
+    closeModalBtnDownside: document.querySelector('[data-basket-modal-close-button]'),
     modal: document.querySelector("[data-basket-modal]"),
   };
 
   refs.openModalBtn.addEventListener("click", openModal(refs.modal));
   refs.openModalMobileBtn.addEventListener("click", openModal(refs.modal));
   refs.closeModalBtn.addEventListener("click", closeModal(refs.modal));
+  refs.closeModalBtnDownside.addEventListener("click", closeModal(refs.modal));
   refs.modal.addEventListener('click', onBackdropClick);
 
 
