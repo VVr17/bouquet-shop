465c3157854c3c989cc40862d1b2dd08ff20bcde
Vira Voronova <vera.voronova@hotmail.com>
2022-09-12 12:45:25 +0200

    updated

--- src/js/helpers/product-lists/catalog-list.js
+++ src/js/helpers/product-lists/catalog-list.js
@@ -1,7 +1,10 @@
 import ProductCards from '../../classes/productCards';
 
+console.log('otside catalog.html')
+
 if (window.location.pathname === '/catalog.html') {
   // in catalog.html
+  console.log('in catalog html')
 
   const catalogCards = new ProductCards({
       listSelector: '#catalog-list',
