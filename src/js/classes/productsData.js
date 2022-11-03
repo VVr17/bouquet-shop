e4d989513878c091c5e2ab2c5e119e684a62c78f
Vira Voronova <vera.voronova@hotmail.com>
2022-11-03 10:29:48 +0100

    api changed

--- src/js/classes/productsData.js
+++ src/js/classes/productsData.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
 export default class ProductsData {
+  // #BASE_URL = ' https://sheet.best/api/sheets/c18f7bf8-af73-4384-ad69-39b8dfc13c08'; // GOOGLE SHEET
+  #BASE_URL = 'https://sheetdb.io/api/v1/qjso21gugm7yo'; // SHEET DB
+
   constructor() {
     this.perPage = 8;
     this.offset = 0;
@@ -10,11 +13,20 @@ export default class ProductsData {
   fetchProductsData = async () => {
     try {
       //link to Google sheets
+      // const response = await axios.get(
+      //   `https://sheet.best/api/sheets/c18f7bf8-af73-4384-ad69-39b8dfc13c08?_limit=${this.perPage}&_offset=${this.offset}`
+      // );
+      // if (!this.totalItems)
+      //   this.totalItems = parseInt(response.headers['x-connection-rows']);
+
       const response = await axios.get(
-        `https://sheet.best/api/sheets/c18f7bf8-af73-4384-ad69-39b8dfc13c08?_limit=${this.perPage}&_offset=${this.offset}`
+        `${this.#BASE_URL}?limit=${this.perPage}&offset=${this.offset}`
       );
-      if (!this.totalItems)
-        this.totalItems = parseInt(response.headers['x-connection-rows']);
+      if (!this.totalItems) {
+        const total = await axios.get(`${this.#BASE_URL}`);
+        this.totalItems = total.data.length;
+      }
+
       return response.data;
     } catch (error) {
       console.warn('fetch-error', error);
@@ -24,11 +36,15 @@ export default class ProductsData {
   fetchProductsDataByProperty = async property => {
     try {
       //link to Google
+      // const response = await axios.get(
+      //   `https://sheet.best/api/sheets/c18f7bf8-af73-4384-ad69-39b8dfc13c08/${property}/true?_limit=${this.perPage}&_offset=${this.offset}`
+      // );
+      // if (!this.totalItems)
+      // this.totalItems = parseInt(response.headers['x-connection-rows']);
       const response = await axios.get(
-        `https://sheet.best/api/sheets/c18f7bf8-af73-4384-ad69-39b8dfc13c08/${property}/true?_limit=${this.perPage}&_offset=${this.offset}`
+        `${this.#BASE_URL}/search?${property}=true`
       );
-      if (!this.totalItems)
-        this.totalItems = parseInt(response.headers['x-connection-rows']);
+
       return response.data;
     } catch (error) {
       console.warn('fetch-error', error);
@@ -38,9 +54,10 @@ export default class ProductsData {
   fetchProductDataById = async id => {
     try {
       //link to Google
-      const response = await axios.get(
-        `https://sheet.best/api/sheets/c18f7bf8-af73-4384-ad69-39b8dfc13c08/id/${id}`
-      );
+      // const response = await axios.get(
+      //   `https://sheet.best/api/sheets/c18f7bf8-af73-4384-ad69-39b8dfc13c08/id/${id}`
+      // );
+      const response = await axios.get(`${this.#BASE_URL}/search?id=${id}`);
       return response.data;
     } catch (error) {
       console.warn('fetch-error', error);
